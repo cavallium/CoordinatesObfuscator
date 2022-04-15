cc18aa427259f34e0c46015dd7270ccf3f4793e8
HarvelsX <90945793+HarvelsX@users.noreply.github.com>
2022-04-15 05:30:52 +0300

    Reformat code & create basic .editorconfig; Please export code style to projects repositories;

--- README.md
+++ README.md
@@ -3,9 +3,10 @@ CoordinatesObfuscator
 
 **Hide the real coordinates to the players.**
 
-
-This plugin helps the owner to make sure that players with modded clients do not take unfair advantage of vanilla players when `/gamerule reducedDebugInfo` is set to `true`.
+This plugin helps the owner to make sure that players with modded clients do not take unfair advantage of vanilla
+players when `/gamerule reducedDebugInfo` is set to `true`.
 
 How it works
 ------------
-Every time a player changes position, dies, joins a server or changes a world, the **X** and **Z** coordinates will be shifted by a random number, making useless every tool that uses absolute coordinates systems.
+Every time a player changes position, dies, joins a server or changes a world, the **X** and **Z** coordinates will be
+shifted by a random number, making useless every tool that uses absolute coordinates systems.
--- src/main/java/org/warp/coordinatesobfuscator/TranslatorServerbound.java
+++ src/main/java/org/warp/coordinatesobfuscator/TranslatorServerbound.java
@@ -11,81 +11,81 @@ import java.util.logging.Logger;
 
 public class TranslatorServerbound {
 
-    private static final String USE_ITEM = "BLOCK_PLACE";
-    private static final String BLOCK_PLACE = "USE_ITEM";
+	private static final String USE_ITEM = "BLOCK_PLACE";
+	private static final String BLOCK_PLACE = "USE_ITEM";
 
-    public static void incoming(Logger logger, final PacketContainer packet, final Player player) {
-        if (player == null || player instanceof TemporaryPlayer) {
-            return;
-        }
-        CoordinateOffset offset = PlayerManager.getOffsetOrJoinPlayer(player, player.getWorld());
-        Objects.requireNonNull(offset);
-        switch (packet.getType().name()) {
-            case "POSITION":
-            case "VEHICLE_MOVE":
-            case "POSITION_LOOK":
-                PlayerManager.setLastPlayerLocation(player, player.getLocation());
-                recvDouble(logger, packet, offset);
-                break;
-            case "STRUCT":
-            case "SET_JIGSAW":
-            case "SET_COMMAND_BLOCK":
-            case "UPDATE_SIGN":
-            case "BLOCK_DIG":
-                recvPosition(logger, packet, offset);
-                break;
-            case USE_ITEM:
-                break;
-            case BLOCK_PLACE:
-                recvMovingPosition(logger, packet, offset);
-                break;
-            case "USE_ENTITY":
-                break;
-            default:
-                break;
-        }
-    }
+	public static void incoming(Logger logger, final PacketContainer packet, final Player player) {
+		if (player == null || player instanceof TemporaryPlayer) {
+			return;
+		}
+		CoordinateOffset offset = PlayerManager.getOffsetOrJoinPlayer(player, player.getWorld());
+		Objects.requireNonNull(offset);
+		switch (packet.getType().name()) {
+			case "POSITION":
+			case "VEHICLE_MOVE":
+			case "POSITION_LOOK":
+				PlayerManager.setLastPlayerLocation(player, player.getLocation());
+				recvDouble(logger, packet, offset);
+				break;
+			case "STRUCT":
+			case "SET_JIGSAW":
+			case "SET_COMMAND_BLOCK":
+			case "UPDATE_SIGN":
+			case "BLOCK_DIG":
+				recvPosition(logger, packet, offset);
+				break;
+			case USE_ITEM:
+				break;
+			case BLOCK_PLACE:
+				recvMovingPosition(logger, packet, offset);
+				break;
+			case "USE_ENTITY":
+				break;
+			default:
+				break;
+		}
+	}
 
-    private static void recvDouble(Logger logger, final PacketContainer packet, final CoordinateOffset offset) {
-        if (packet.getDoubles().size() > 2) {
-            packet.getDoubles().modify(0, x -> x == null ? null : x + offset.getX());
-            packet.getDoubles().modify(2, z -> z == null ? null : z + offset.getZ());
-        } else {
-            int size = packet.getDoubles().size();
-            logger.severe("Packet size error: " + size);
-        }
-    }
+	private static void recvDouble(Logger logger, final PacketContainer packet, final CoordinateOffset offset) {
+		if (packet.getDoubles().size() > 2) {
+			packet.getDoubles().modify(0, x -> x == null ? null : x + offset.getX());
+			packet.getDoubles().modify(2, z -> z == null ? null : z + offset.getZ());
+		} else {
+			int size = packet.getDoubles().size();
+			logger.severe("Packet size error: " + size);
+		}
+	}
 
 
-    private static void recvInt(Logger logger, final PacketContainer packet, final CoordinateOffset offset, final int index) {
-        if (packet.getIntegers().size() > 2) {
-            packet.getIntegers().modify(index, curr_x -> curr_x == null ? null : curr_x + offset.getXInt());
-            packet.getIntegers().modify(index + 2, curr_z -> curr_z == null ? null : curr_z + offset.getZInt());
-        } else {
-            logger.severe("Packet size error");
-        }
-    }
+	private static void recvInt(Logger logger, final PacketContainer packet, final CoordinateOffset offset, final int index) {
+		if (packet.getIntegers().size() > 2) {
+			packet.getIntegers().modify(index, curr_x -> curr_x == null ? null : curr_x + offset.getXInt());
+			packet.getIntegers().modify(index + 2, curr_z -> curr_z == null ? null : curr_z + offset.getZInt());
+		} else {
+			logger.severe("Packet size error");
+		}
+	}
 
-    private static void recvPosition(Logger logger, final PacketContainer packet, final CoordinateOffset offset) {
-        if (packet.getBlockPositionModifier().size() > 0) {
-            packet.getBlockPositionModifier().modify(0, pos -> offsetPosition(logger, offset, pos));
-        } else {
-            logger.severe("Packet size error");
-        }
-    }
+	private static void recvPosition(Logger logger, final PacketContainer packet, final CoordinateOffset offset) {
+		if (packet.getBlockPositionModifier().size() > 0) {
+			packet.getBlockPositionModifier().modify(0, pos -> offsetPosition(logger, offset, pos));
+		} else {
+			logger.severe("Packet size error");
+		}
+	}
 
-    private static BlockPosition offsetPosition(Logger logger, CoordinateOffset offset, BlockPosition pos) {
-        if (pos == null) return null;
-        return pos.add(new BlockPosition(offset.getXInt(), 0, offset.getZInt()));
-    }
+	private static BlockPosition offsetPosition(Logger logger, CoordinateOffset offset, BlockPosition pos) {
+		if (pos == null) return null;
+		return pos.add(new BlockPosition(offset.getXInt(), 0, offset.getZInt()));
+	}
 
-    private static void recvMovingPosition(Logger logger, final PacketContainer packet, final CoordinateOffset offset) {
-        var mopb = packet.getMovingBlockPositions().read(0);
-        if (mopb == null) {
-            return;
-        }
-        mopb.setBlockPosition(mopb.getBlockPosition().add(new BlockPosition(offset.getXInt(), 0, offset.getZInt())));
-        mopb.setPosVector(mopb.getPosVector().add(new Vector(offset.getX(), 0, offset.getZ())));
-        packet.getMovingBlockPositions().write(0, mopb);
-    }
+	private static void recvMovingPosition(Logger logger, final PacketContainer packet, final CoordinateOffset offset) {
+		var mopb = packet.getMovingBlockPositions().read(0);
+		if (mopb == null) {
+			return;
+		}
+		mopb.setBlockPosition(mopb.getBlockPosition().add(new BlockPosition(offset.getXInt(), 0, offset.getZInt())));
+		mopb.setPosVector(mopb.getPosVector().add(new Vector(offset.getX(), 0, offset.getZ())));
+		packet.getMovingBlockPositions().write(0, mopb);
+	}
 }
